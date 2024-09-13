9245e519f1267ccd7341af1fe51d1c8f56665ce0
bella-sheridan <isabellasheridan1@gmail.com>
2024-09-13 10:39:31 -0500

    test

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareSoftware.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareSoftware.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
-//import com.kauailabs.navx.ftc.AHRS;
+import com.kauailabs.navx.ftc.AHRS;
 import com.qualcomm.hardware.kauailabs.NavxMicroNavigationSensor;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -13,10 +13,6 @@ public class HardwareSoftware {
 
     private HardwareMap hw = null;
 
-    Servo LeftHangServo = null;
-    DcMotor HangMotor = null;
-
-    Servo RightHangServo = null;
 
 
     DcMotorEx FRdrive    = null;
@@ -24,41 +20,7 @@ public class HardwareSoftware {
     DcMotorEx BLdrive      = null;
     DcMotorEx FLdrive     = null;
 
-    DcMotorEx Linear     = null;
-
-    CRServo Conveyor = null;
-
-    Servo Grippy = null;
-    Servo Flippy = null;
-    Servo Plane = null;
-
-    Servo PurplePixel = null;
-
-
-    DcMotorEx Lodom = null;
-    DcMotorEx Rodom = null;
-
-
-
-//    AHRS gyro = null;
-
-    public double GripClose = 0.7;
-    public double GripOpen = 0.4;
-
-    public double FlipUp = 0.8;
-    public double FlipMid = 0.4;
-    public double FlipDown = 0.175;
-
-    public double LeftHangServoReleased = 0;
-    public double LeftHangServoDown = 0.55;
-
-    public double RightHangServoReleased = 1;
-    public double RightHangServoDown = 0.45;
-
-    public double PurplePixelDown = -0.2;
-    public double PurplePixelReleased = 0.95;
-
-
+    AHRS gyro = null;
 
 
     public void init(HardwareMap ahw){
@@ -71,72 +33,24 @@ public class HardwareSoftware {
         BLdrive = hw.get(DcMotorEx.class, "BLdrive");
         BRdrive = hw.get(DcMotorEx.class, "BRdrive");
 
-        Linear = hw.get(DcMotorEx.class, "Linear");
-
-        Rodom = hw.get(DcMotorEx.class, "Rodom");
-        Lodom = hw.get(DcMotorEx.class, "Lodom");
-
-        Grippy = hw.get(Servo.class, "Grippy");
-        Flippy = hw.get(Servo.class, "Flippy");
-        Plane = hw.get(Servo.class, "Plane");
-
-        Conveyor = hw.get(CRServo.class, "Conveyor");
-
-        LeftHangServo = hw.get(Servo.class, "LeftHangServo");
-        HangMotor = hw.get(DcMotor.class, "HangMotor");
-
-        RightHangServo = hw.get(Servo.class, "RightHangServo");
-
-        PurplePixel = hw.get(Servo.class, "PurplePixel");
-
-
-//        gyro = AHRS.getInstance(hw.get(NavxMicroNavigationSensor.class, "gyro"),
-//                AHRS.DeviceDataType.kProcessedData);
-
         FLdrive.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
         BRdrive.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
         FRdrive.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
         BLdrive.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
 
-        //back odometry, right odometry, left odometry
-        Rodom.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        Lodom.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-
-        Linear.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        Linear.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        Flippy.setDirection(Servo.Direction.REVERSE);
-
-        HangMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-
         FLdrive.setDirection(DcMotorEx.Direction.REVERSE);
         FRdrive.setDirection(DcMotorEx.Direction.FORWARD);
-        BLdrive.setDirection(DcMotorEx.Direction.REVERSE);
-        BRdrive.setDirection(DcMotorEx.Direction.FORWARD);
-
-
+        BLdrive.setDirection(DcMotorEx.Direction.FORWARD);
+        BRdrive.setDirection(DcMotorEx.Direction.REVERSE);
 
         FLdrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BRdrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         FRdrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BLdrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        Linear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        gyro = AHRS.getInstance(hw.get(NavxMicroNavigationSensor.class, "gyro"),
+                AHRS.DeviceDataType.kProcessedData);
 
-        Rodom.setMode(DcMotor.RunMode.RESET_ENCODERS);
-        Lodom.setMode(DcMotor.RunMode.RESET_ENCODERS);
-
-        Linear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        LeftHangServo.setPosition(LeftHangServoDown);
-        RightHangServo.setPosition(RightHangServoDown);
-
-        Grippy.setPosition(0.7);
-
-
-
-        PurplePixel.setPosition(PurplePixelDown);
 
     }
 
@@ -156,38 +70,7 @@ public class HardwareSoftware {
         return BRdrive;
     }
 
-    public DcMotorEx Rodom(){
-        return Rodom;
-    }
-    public DcMotorEx Lodom(){
-        return Lodom;
-    }
-
-//    public AHRS gyro(){return gyro;}
-
-    public DcMotorEx Linear(){
-        return Linear;
-    }
-
-    public Servo Flippy(){
-        return Flippy;
-    }
-    public Servo Grippy(){
-        return Grippy;
-    }
-    public Servo Plane(){return Plane;}
-
-    public CRServo Conveyor(){
-        return Conveyor;
-    }
-
-    public Servo LeftHangServo(){return LeftHangServo;}
-    public DcMotor HangMotor(){return HangMotor;}
-
-    public Servo RightHangServo(){return RightHangServo;}
-
-    public Servo PurplePixel(){return PurplePixel;}
-
+    public AHRS gyro(){return gyro;}
 
 }
 
